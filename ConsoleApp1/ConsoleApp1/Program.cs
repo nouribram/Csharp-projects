e37b81a2b7084a81f0fefa4da48a4b11988c92b3
nouribram <nourtesla@yahoo.com>
2025-04-04 15:09:35 +0200

    final update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int result;
+            
+            Console.WriteLine("Welcome to the program");
+            Console.WriteLine("Please enter the first number");
+           
+             int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number");
+
+             int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the operator + * - / ");
+
+             string op = Console.ReadLine();
+
+            if (op == "+")
+                {
+                 result = number1 + number2;
+            }
+            else if (op == "-")
+            {
+                result = number1 - number2;
+            }
+            else if (op == "*")
+            {
+                result = number1 * number2;
+            }
+            else
+            {
+            result = number1 / number2;
+            }
+
+            Console.WriteLine("The answer is" + result);
+
+                Console.ReadKey();  
         }
     }
 }
