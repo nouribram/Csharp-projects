b5ab5b6ffc9a5b0db274717b20bd5192de0b0375
nouribram <nourtesla@yahoo.com>
2025-05-08 19:06:42 +0300

    new

---
+++ myapp/myapp/Controllers/ItemsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item
+            return View();
+        }
+    }
+}
---
+++ myapp/myapp/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace myapp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+}
