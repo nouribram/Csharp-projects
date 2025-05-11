e51c774c79a0fce9d159e9e2dc6228804a2d7e14
nouribram <nourtesla@yahoo.com>
2025-05-11 14:53:10 +0300

    new update

---
+++ C-A/C-A/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace C_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            float result;
+
+            Console.WriteLine("To find the area of rectangle press r, for circle press any key");
+
+            answer = Console.ReadLine();
+
+            if (answer == "r")
+            {
+                Console.WriteLine("Please enter the H of R");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter W of R");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+
+            else 
+            {
+                Console.WriteLine("please enter the radius of the circle");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius *  radius);
+               
+            }
+
+            Console.WriteLine("The Result is" + result);
+            Console.ReadKey();
+        }
+    }
+}
